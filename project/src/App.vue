698d0799a9b037acebd1cd1b22ec02a6c9e24965
Arthur Pechenkin <freakn@Arthurs-MacBook-Air.local>
2023-07-02 03:41:04 +0700

    add smooth

--- project/src/App.vue
+++ project/src/App.vue
@@ -32,15 +32,15 @@ const slides = [
             Я опытный веб-дизайнер и разработчик коммерческих сайтов. Сделаю ваш сайт красивым и заставлю его продавать.
         </p>
         <div class="flex gap-2 font-bold italic">
-            <a href="#about">Обо мне</a>
+            <a href="#about" class="hover:underline">Обо мне</a>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
             </svg>
-            <a href="#demo">Мои работы</a>
+            <a href="#demo" class="hover:underline">Мои работы</a>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
             </svg>
-            <a href="#contact">Контакты</a>
+            <a href="#contact" class="hover:underline">Контакты</a>
         </div>
         <div class="arrow-down absolute bottom-6">
             <span></span>
